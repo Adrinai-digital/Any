73e60c7571e99deade76889cda3c11fc137217d8
conchi-Any <adrinai.conchi@hotmail.com>
2026-01-14 16:19:07 +0100

    server

--- server.js
+++ server.js
@@ -8,9 +8,8 @@ const bodyParser = require('body-parser');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
-const citasRoutes = require("./routes/citasRoutes");
+const citasRoutes = require('./routes/citasRoutes');
 const PORT = process.env.PORT || 3000;
-
 const express = require('express');
 const app = express();
 
@@ -96,8 +95,8 @@ app.use(cors({
     origin: 'https://autoconocimientoygratitud.com',
     credentials: true
 }));
-app.use("/api/citas", citasRoutes);
-app.use("/curso", citasRoutes); // para la ruta /curso/metodo-learn
+app.use('/api/citas', citasRoutes);
+app.use('/curso', citasRoutes); // para la ruta /curso/metodo-learn
 //app.use(express.json());
 //app.use(bodyParser.json());
  // importante para leer body en POST
@@ -107,8 +106,6 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use("/api/citas", require("./routes/citas"));
 
 // Ruta de registro
 app.post('/register', async (req, res) => {
