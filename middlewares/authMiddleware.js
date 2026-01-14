177189a0d8cf78354530d5633c77535acffcf2d0
conchi-Any <adrinai.conchi@hotmail.com>
2026-01-14 17:18:25 +0100

    authmiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,23 +4,16 @@ const SECRET = process.env.JWT_SECRET;
 function authMiddleware(req, res, next) {
   let token = null;
 
-  // 1️⃣ API (fetch / axios)
   if (req.headers.authorization) {
     token = req.headers.authorization.split(" ")[1];
   }
 
-  // 2️⃣ Navegación normal (cookies)
   if (!token && req.cookies?.token) {
     token = req.cookies.token;
   }
 
   if (!token) {
-    // 🔥 diferencia API vs VISTA
-    if (req.originalUrl.startsWith("/api")) {
-      return res.status(401).json({ error: "No autenticado" });
-    } else {
-      return res.redirect("/login");
-    }
+    return res.redirect("/login");
   }
 
   try {
@@ -28,11 +21,7 @@ function authMiddleware(req, res, next) {
     req.user = user;
     next();
   } catch (err) {
-    if (req.originalUrl.startsWith("/api")) {
-      return res.status(403).json({ error: "Token inválido" });
-    } else {
-      return res.redirect("/login");
-    }
+    return res.redirect("/login");
   }
 }
 
